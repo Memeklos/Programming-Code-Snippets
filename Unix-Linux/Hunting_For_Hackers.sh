c329ca88290289bd1e6e78e65f05fad8d8d4559a
Memeklos <mariel.klosterman@trojans.dsu.edu>
2021-02-16 23:18:38 -0800

    Changing > to cp command

--- Unix-Linux/Hunting_For_Hackers.sh
+++ Unix-Linux/Hunting_For_Hackers.sh
@@ -8,7 +8,7 @@ echo "------Hunting For Hackers Script------"
 
 ## Problem 1 ##
 # Copy /var/log/auth.log to a new file
-/var/log/auth.log > UserLog.txt
+cp /var/log/auth.log UserLog.txt
 # Filter out lines with the standard IP address
 grep -v "192.168.1.69" UserLog.txt > SusLog # && mv tmpfile UserLog.txt
 # Filter by lines that contain the 192.168. portion
@@ -21,7 +21,7 @@ grep "penelope" /etc/group
 
 ## Problem 3 ##
 # Copy /etc/passwd to a new file
-/etc/passwd > FindUserRoot.txt
+cp /etc/passwd FindUserRoot.txt
 # Filter out lines with a UID of 0
 grep ":x:0" FindUserRoot.txt
 
