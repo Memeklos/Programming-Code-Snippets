ee169bcaaec9d13a18bcfc22a1f5c1e49f136e0a
Memeklos <mariel.klosterman@trojans.dsu.edu>
2021-04-23 10:58:27 -0700

    Adding Backup_Script.sh

---
+++ Bash/Backup_Script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## Purpose ##
+## This script takes two arguments 1) backup or restore, 2) directory and performs the action on the given directory. ##
+
+## Header ##
+# Prints the header
+echo "------Backup Script------"
+
+## Commands ##
+# Sets the info variable to yellow
+info=$'\u001b[33m'
+# Sets the prep variable to cyan
+prep=$'\u001b[36m'
+# Sets the complete variable to green
+complete=$'\e[1;32m'
+# Sets the error variable to red
+error=$'\033[0;31m'
+
+## Backup or Restore Directory ##
+# If the value is "backup", compress the given directory
+if [ $1 == "backup" ]
+then
+    # Sets the dname variable to the given directory name and removes the trailing slash
+    dname=${2%/}
+    # Prints that the following files and directories will be affected
+    echo "$info"
+    echo "The following files and directories will be compressed to $dname.tar:"
+    echo "$prep"
+    # Compresses the given directory name to a tar file with the directory name
+    tar cJfav $dname.tar $2
+    # Prints that the backup is complete
+    echo "$complete"
+    echo "Backup complete!"
+# If the value is "restore", decompress the given directory
+elif [ $1 == "restore" ]
+then
+    # Sets the dname variable to the given directory name and removes the trailing slash
+    dname=${2%/}
+    # Prints that the following files and directories will be affected
+    echo "$info"
+    echo "The following files and directories will be decompressed to $dname/:"
+    echo "$prep"
+    # Compresses the given directory name to a tar file with the directory name
+    tar xJfv $dname.tar
+    # Prints that the restore is complete
+    echo "$complete"
+    echo "Restore complete!"
+else
+    echo "$error" && echo "Please enter backup or restore as your first argument"
+fi
