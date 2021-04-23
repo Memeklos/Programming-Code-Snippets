44c0706059035ae8083bac709bf0fe3283ccd4c5
Memeklos <mariel.klosterman@trojans.dsu.edu>
2021-04-23 11:03:23 -0700

    Updating comments

--- Bash/Backup_Script.sh
+++ Bash/Backup_Script.sh
@@ -23,28 +23,34 @@ if [ $1 == "backup" ]
 then
     # Sets the dname variable to the given directory name and removes the trailing slash
     dname=${2%/}
-    # Prints that the following files and directories will be affected
+    # Sets the text color for the following lines
     echo "$info"
+    # Prints that the following files and directories will be affected
     echo "The following files and directories will be compressed to $dname.tar:"
+    # Sets the text color for the following lines
     echo "$prep"
     # Compresses the given directory name to a tar file with the directory name
     tar cJfav $dname.tar $2
-    # Prints that the backup is complete
+    # Sets the text color for the following lines
     echo "$complete"
+    # Prints that the backup is complete
     echo "Backup complete!"
 # If the value is "restore", decompress the given directory
 elif [ $1 == "restore" ]
 then
     # Sets the dname variable to the given directory name and removes the trailing slash
     dname=${2%/}
-    # Prints that the following files and directories will be affected
+    # Sets the text color for the following lines
     echo "$info"
+    # Prints that the following files and directories will be affected
     echo "The following files and directories will be decompressed to $dname/:"
+    # Sets the text color for the following lines
     echo "$prep"
     # Decompresses the given directory name from a tar file with the directory name
     tar xJfv $dname.tar
-    # Prints that the restore is complete
+    # Sets the text color for the following lines
     echo "$complete"
+    # Prints that the restore is complete
     echo "Restore complete!"
 else
     echo "$error" && echo "Please enter backup or restore as your first argument"
