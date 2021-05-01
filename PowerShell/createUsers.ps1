bd0d023fd1010ddc56d046e537c67c96cdc9b5c1
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-01 11:33:39 -0500

    Update createUsers.ps1

--- PowerShell/createUsers.ps1
+++ PowerShell/createUsers.ps1
@@ -1,26 +1,15 @@
-Create Local User Accounts
-
 ## This script creates new local user accounts ##
 
 # Takes a filename as a parameter
+$fname=$args[0]
 
-
+# Read in usernames from the file
+$file=Get-Content -Path $fname
 
 # Takes the password from the user
-
-ForEach-Object { Add-Content -Path .\LineNumbers.txt -Value "This is line $_." }
-Get-Content -Path .\LineNumbers.txt
-
-# Uses the same default password for each account
-
-
-
+$pass=Read-Host "Enter the user password" -AsSecureString
 
 # Creates a local user account for each user in the text file
-
-New-LocalUser -Name "User02" -Description "Description of this account."
-
-
-
-
-Get-User -Identity "Coy Damon" | Format-List
+foreach ($i in $file) {
+    New-LocalUser -Name "$i" -Password $pass -Description "A user from the $fname file."
+}
