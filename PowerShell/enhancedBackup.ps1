cf098cc0ed4f4af916bf20e5fd052313aa2cb3d0
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-01 08:22:19 -0500

    Update enhancedBackup.ps1

--- PowerShell/enhancedBackup.ps1
+++ PowerShell/enhancedBackup.ps1
@@ -1,25 +1,25 @@
 ## This script backs up all given files in the current directory with a given extension into a separate folder ##
 
-# Takes an extension as a script argument
+# Sets the ext variable to the first argument
 $ext=$args[0]
+# Sets the folder variable to the string "backup"
 $folder="backup"
-Get-ChildItem ./ | Select-Object Name
 
-Get-ChildItem ./ | where-object {$_.Extension -eq '.$extension'}
-
-# Creates the folder backup if necessary
+# If the directory is already created, prints that it exists
 if (Test-Path -path $folder -IsValid) {
     Write-Host "The directory $folder already exists"
 }
+# Otherwise, creates the directory and prints that it has been created
 else {
     New-Item -Name "$folder" -ItemType "directory"
     Write-Host "The directory $folder has been created"
 }
 
+#Get-ChildItem ./ | Select-Object Name
+
 # Within the current directory, copy all files matching the argument to a folder called backup
-Foreach ($file in $folder) {
-    copy $fname backup/$fname
-}
+Get-ChildItem ./ | Where-Object {$_.Extension -eq '.$extension'} | Foreach-Object
+
 
 # Tell the user which files were backed up
 
