db5f0a86c1f211532f149ad6e6cacc4c689fb827
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-04-30 22:09:30 -0500

    Update backup.ps1

--- PowerShell/backup.ps1
+++ PowerShell/backup.ps1
@@ -1,7 +1,7 @@
 ## This script back up the given file by making a copy with the .backup extension ##
 
 # Takes the file name as a script argument
-
+Write-output $args
 
 
 # Makes a copy of that file and add “.backup” to the name
