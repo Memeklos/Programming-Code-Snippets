48bcf76e57ec6ac82db304ed7b4c8b376a4949f8
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 15:38:48 -0500

    Update createVM.ps1

--- PowerShell/createVM.ps1
+++ PowerShell/createVM.ps1
@@ -1,5 +1,16 @@
 # Connect to the server
-Connect-VIServer -Server 192.168.1.10 -Protocol https -User root -Password Password1!
-Connect-VIServer Server -Credential $myCredentialsObject -Port 1234
-Connect-VIServer "Server" -SessionId $sessionId
-Connect-VIServer Server
+Connect-VIServer -Server 192.168.1.10 -Protocol https -User root -Password Password1! -Force
+#Connect-VIServer 192.168.1.10
+
+# Upload the Ubuntu ISO
+$ds = Get-Datastore datastore1
+New-PSDrive -Name ds1 -PSProvider ViMdatastore -Root '\' -location $ds
+Copy-DatastoreItem C:\Users\DSU\Desktop\ubuntu-20.04.2.0-desktop-amd64.iso -Destination ds1:\ubuntu.iso
+
+# Create the VM
+$ubu = New-VM -Name UbU -Datastore Datastore1 -DiskGB 20 -DiskStorageFormat Thin -MemoryGB 4 -CD -GuestId UbuntuGuest -NumCpu 2
+$cd = New-CDDrive -VM $ubu -IsoPath "[datastore1] cds/ubuntu.iso"
+Start-VM $ubu
+
+# Create a snapshot
+New-Snapshot -VM $ubu -Name "AfterUpdate"
