7f412efdd55df2c4dcabc631a6ad1403be6676c0
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-01 23:59:57 -0500

    Add files via upload

---
+++ PowerShell/enhancedbackup.tests.ps1
@@ -0,0 +1,23 @@
+﻿# Enhanced Backup Test
+param (
+    [Parameter(Mandatory)]
+    [string] $Path
+)
+
+BeforeAll { 
+    . $PSScriptRoot/enhancedbackup.ps1 $Path
+}
+
+Describe 'Enhanced Backup' {
+    It 'Returns the files with the given extension' {
+        $folder="backup"
+        $output = Get-ChildItem ./$folder | Where-Object {$_.Extension -eq "$Path"} | ForEach-Object { Write-Host $_.Name }
+        foreach ($file in $output) {
+            $file | Should -Be "$file.$Path"
+        }
+    }
+    It 'Returns that the folder backup exists' {
+        $output = Test-Path -path $folder
+        $output | Should -Be "true"
+    }
+}
