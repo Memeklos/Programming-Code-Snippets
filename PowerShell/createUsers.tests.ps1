8a6ab28ae32f2fb7c6e1a2cab5a629b6a9eef684
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 00:37:27 -0500

    Add files via upload

---
+++ PowerShell/createUsers.tests.ps1
@@ -0,0 +1,18 @@
+﻿# Create Users Test
+param (
+    [Parameter(Mandatory)]
+    [string] $Path
+)
+
+BeforeAll { 
+    . $PSScriptRoot/createUsers.ps1 $Path
+}
+
+Describe 'Create Users' {
+    It 'Returns the usernames of the users that have been created.' {
+        $file=Get-Content -Path $Path
+        foreach ($user in $file) {
+            Get-LocalUser -Name $user | Should -Be "$user"
+        }
+    }
+}
