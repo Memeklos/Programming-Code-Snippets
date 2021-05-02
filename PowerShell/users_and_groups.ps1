ee50ccf694893838516e19bcc0312031a0432921
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 18:51:49 -0500

    Update users_and_groups.ps1

--- PowerShell/users_and_groups.ps1
+++ PowerShell/users_and_groups.ps1
@@ -120,3 +120,19 @@ foreach ($User in $ADUsers) {
         Write-Host "The user account $username is created." -ForegroundColor Cyan
     }
 }
+
+$import_users | ForEach-Object {
+    New-ADUser `
+        -Name $($_.FirstName + " " + $_.LastName) `
+        -GivenName $_.FirstName `
+        -Surname $_.LastName `
+        -Department $_.Department `
+        -State $_.State `
+        -EmployeeID $_.EmployeeID `
+        -DisplayName $($_.FirstName + " " + $_.LastName) `
+        -Office $_.Office `
+        -UserPrincipalName $_.UserPrincipalName `
+        -SamAccountName $_.SamAccountName `
+        -AccountPassword $(ConvertTo-SecureString $_.Password -AsPlainText -Force) `
+        -Enabled $True
+}
