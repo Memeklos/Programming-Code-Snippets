523705d04cf97ec2815a3796c9a7c001a2185a98
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-01 22:43:02 -0500

    Add files via upload

---
+++ PowerShell/backup.tests.ps1
@@ -0,0 +1,16 @@
+﻿# Backup Test
+param (
+    [Parameter(Mandatory)]
+    [string] $Path
+)
+
+BeforeAll { 
+    . $PSScriptRoot/backup.ps1 $Path
+}
+
+Describe 'Backup' {
+    It 'Returns the name of the file that is created from the backup script' {
+        $output = Get-ChildItem ./ | Where-Object {$_.Name -eq "$Path.backup"}
+        $output | Should -Be "$Path.backup"
+    }
+}
