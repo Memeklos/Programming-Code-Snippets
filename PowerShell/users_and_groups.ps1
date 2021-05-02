b430a11c6102970004f34c17747d18cff4fdb87c
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 16:24:45 -0500

    Update users_and_groups.ps1

--- PowerShell/users_and_groups.ps1
+++ PowerShell/users_and_groups.ps1
@@ -10,3 +10,12 @@ $pass = Read-Host "Enter User Password" - AsSecureString
 
 # Create the appropriate groups based on department
 
+if (Get-ADUser -F { SamAccountName -eq $username }) {
+        
+        # If user does exist, give a warning
+        Write-Warning "A user account with username $username already exists in Active Directory."
+    }
+    else {
+
+        # User does not exist then proceed to create the new user account
+    }
