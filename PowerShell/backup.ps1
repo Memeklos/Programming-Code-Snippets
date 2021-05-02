5dbfd6a8f52dcd33bec3276cd9b0c0cc0b91c1f5
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-01 22:41:52 -0500

    Update backup.ps1

--- PowerShell/backup.ps1
+++ PowerShell/backup.ps1
@@ -1,9 +1,12 @@
 ## This script back up the given file by making a copy with the .backup extension ##
-# set-executionpolicy -executionpolicy bypass -scope currentuser
-# Takes the file name as a script argument
+# Stops the script if an error occurs
+$ErrorActionPreference = "Stop"
+# Sets the fname variable to the given file name
 $fname=$args[0]
 
 # Copy the file to a new file with the .backup extension
 Copy-Item $fname "$fname.backup"
 # Check and then print out that file has been copied
-Get-ChildItem ./ | Where-Object {$_.Extension -eq ".backup"} | ForEach-Object { Write-Host "Backed up" $_.Name }
+Get-ChildItem ./ | Where-Object {$_.Extension -eq "$fname.backup"}
+# Prints that the file has been backed up
+Write-Host "Backed up $fname.backup"
