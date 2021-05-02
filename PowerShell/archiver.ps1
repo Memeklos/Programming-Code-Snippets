36e8326cf148e34c6d72a5a0d5715f59e996324f
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-01 22:38:57 -0500

    Update archiver.ps1

--- PowerShell/archiver.ps1
+++ PowerShell/archiver.ps1
@@ -1,13 +1,11 @@
 ## This script zip up any file in the current directory that hasn’t been modified in the given day/s ##
-
+$ErrorActionPreference = "Stop"
 # Sets the days variable to the passed argument
 $days=$args[0]
 # Sets the today variable to the current date in the specified format
 $today=Get-Date -Format "MM-dd-yyyy"
 
-# Prints that the zip file is being created
-Write-Host "Creating zip file"
 # Finds the files with a modification date earlier than $days and sends them to an archived directory
 Get-ChildItem ./ | Where{$_.LastWriteTime -lt (Get-Date).AddDays(-$days)} | Compress-Archive -CompressionLevel Optimal -DestinationPath "$today.Zip"
 # Checks the folder for the name of the archive folder and prints that it has been created
-Get-ChildItem ./ | Where-Object {$_.Name -eq "$today.Zip"} | ForEach-Object { Write-Host $_.Name "has been created" }
+Get-ChildItem ./ | Where-Object {$_.Name -eq "$today.Zip"} | ForEach-Object { Write-Host $_.Name "created" }
