5c69cb67eb3de6df9a88592ec4cc27d18d2a5be0
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 22:31:23 -0500

    Update and rename process-listing.ps1 to hotfix-checker.ps1

---
+++ PowerShell/hotfix-checker.ps1
@@ -0,0 +1,7 @@
+#
+# Sets the variable kb to the passed argument
+$kb=$args[0]
+
+# Get list of hotfixes from multiple remote machines (use an array)
+
+# Check to see if the hotfix is installed, output results
--- PowerShell/process-listing.ps1
+++
@@ -1,4 +0,0 @@
-#
-# Pull a process listing from a remote host
-# Get the process listing from local
-# Output the names of the processes that are running on the server but not locally
