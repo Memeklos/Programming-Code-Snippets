69a89728440892724aede72edde70f1bfdd623aa
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 23:20:07 -0500

    Update hotfix-checker.ps1

--- PowerShell/hotfix-checker.ps1
+++ PowerShell/hotfix-checker.ps1
@@ -1,7 +1,19 @@
-#
 # Sets the variable kb to the passed argument
 $kb=$args[0]
+# Sets the 
+$hf=0
 
 # Get list of hotfixes from multiple remote machines (use an array)
-
+$list = Get-Hotfix -ComputerName 192.168.1.2 -Credential XYZ\Administrator | ForEach-Object {
+    Write-Host "Searching..."
+    if ($_.HotFixID -eq $kb){
+        $hf++
+    }
+}
 # Check to see if the hotfix is installed, output results
+if ($hf -eq 1) {
+    Write-Host "$kb was installed"
+}
+else {
+    Write-Host "$kb was not installed"
+}
