ab3d7f58e2291fe1c000e5577b9c61bda6458392
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 20:56:17 -0500

    Update users_and_groups.ps1

--- PowerShell/users_and_groups.ps1
+++ PowerShell/users_and_groups.ps1
@@ -1,138 +1,50 @@
 # Creating Users and Groups
 
-# Take a CSV input
+$ErrorActionPreference = "Stop"
+# Set file variable to the passed argument
+$fname=$args[0]
+# Import CSV
+$file=Import-Csv $fname -Delimiter ","
 
-# Creates User accounts for each user in the CSV with their given password
-# Create User
-New-ADUser "$user"
-# Password
-$pass = Read-Host "Enter User Password" - AsSecureString
-
-# Create the appropriate groups based on department
-
-if (Get-ADUser -F { SamAccountName -eq $username }) {
-        
-        # If user does exist, give a warning
-        Write-Warning "A user account with username $username already exists in Active Directory."
-    }
-    else {
-
-        # User does not exist then proceed to create the new user account
-    }
-    $username = $User.username
-    $password = $User.password
-    $firstname = $User.firstname
-    $lastname = $User.lastname
-    $initials = $User.initials
-    $OU = $User.ou #This field refers to the OU the user account is to be created in
-    $email = $User.email
-    $streetaddress = $User.streetaddress
-    $city = $User.city
-    $zipcode = $User.zipcode
-    $state = $User.state
-    $telephone = $User.telephone
-    $jobtitle = $User.jobtitle
-    $company = $User.company
-    $department = $User.department
-    
--SamAccountName $username `
-            -UserPrincipalName "$username@$UPN" `
-            -Name "$firstname $lastname" `
-            -GivenName $firstname `
-            -Surname $lastname `
-            -Initials $initials `
-            -Enabled $True `
-            -DisplayName "$lastname, $firstname" `
-            -Path $OU `
-            -City $city `
-            -PostalCode $zipcode `
-            -Company $company `
-            -State $state `
-            -StreetAddress $streetaddress `
-            -OfficePhone $telephone `
-            -EmailAddress $email `
-            -Title $jobtitle `
-            -Department $department `
-            -AccountPassword (ConvertTo-secureString $password -AsPlainText -Force) -ChangePasswordAtLogon $True
-
-
-# Import active directory module for running AD cmdlets
-Import-Module ActiveDirectory
-  
-# Store the data from NewUsersFinal.csv in the $ADUsers variable
-$ADUsers = Import-Csv C:\temp\NewUsersFinal.csv -Delimiter ";"
-
-# Define UPN
-$UPN = "exoip.com"
-
-# Loop through each row containing user details in the CSV file
-foreach ($User in $ADUsers) {
-
-    #Read user data from each field in each row and assign the data to a variable as below
-    $username = $User.username
-    $password = $User.password
-    $firstname = $User.firstname
-    $lastname = $User.lastname
-    $initials = $User.initials
-    $OU = $User.ou #This field refers to the OU the user account is to be created in
-    $email = $User.email
-    $streetaddress = $User.streetaddress
-    $city = $User.city
-    $zipcode = $User.zipcode
-    $state = $User.state
-    $telephone = $User.telephone
-    $jobtitle = $User.jobtitle
-    $company = $User.company
-    $department = $User.department
-
-    # Check to see if the user already exists in AD
-    if (Get-ADUser -F { SamAccountName -eq $username }) {
-        
-        # If user does exist, give a warning
-        Write-Warning "A user account with username $username already exists in Active Directory."
-    }
-    else {
-
-        # User does not exist then proceed to create the new user account
-        # Account will be created in the OU provided by the $OU variable read from the CSV file
+# Create User for each entry
+$file | ForEach-Object {
+        Write-Host "Creating user for" $_.Username
         New-ADUser `
-            -SamAccountName $username `
-            -UserPrincipalName "$username@$UPN" `
-            -Name "$firstname $lastname" `
-            -GivenName $firstname `
-            -Surname $lastname `
-            -Initials $initials `
-            -Enabled $True `
-            -DisplayName "$lastname, $firstname" `
-            -Path $OU `
-            -City $city `
-            -PostalCode $zipcode `
-            -Company $company `
-            -State $state `
-            -StreetAddress $streetaddress `
-            -OfficePhone $telephone `
-            -EmailAddress $email `
-            -Title $jobtitle `
-            -Department $department `
-            -AccountPassword (ConvertTo-secureString $password -AsPlainText -Force) -ChangePasswordAtLogon $True
-
-        # If user is created, show message.
-        Write-Host "The user account $username is created." -ForegroundColor Cyan
-    }
-}
-
-$import_users | ForEach-Object {
-    New-ADUser `
-        -Name $($_.FirstName + " " + $_.LastName) `
-        -GivenName $_.FirstName `
-        -Surname $_.LastName `
-        -Department $_.Department `
-        -State $_.State `
-        -EmployeeID $_.EmployeeID `
-        -DisplayName $($_.FirstName + " " + $_.LastName) `
-        -Office $_.Office `
-        -UserPrincipalName $_.UserPrincipalName `
-        -SamAccountName $_.SamAccountName `
-        -AccountPassword $(ConvertTo-SecureString $_.Password -AsPlainText -Force) `
-        -Enabled $True
+            -SamAccountName $_.Username `
+            -Name $_.Username `
+            -GivenName $_.Firstname `
+            -Surname $_.Lastname `
+            -DisplayName $_.Username `
+            -Path $_.Ou `
+            -State $_.Province `
+            -Department $_.Department `
+            -AccountPassword (ConvertTo-secureString $_.Password -AsPlainText -Force) `
+            -Enable $True
+        # If the identity group is LocalAdmins
+        if ($_.Identity -eq "LocalAdmins") {
+            # Add user to the Administrators group
+            Add-ADGroupMember -Identity Administrators -Members $_.Username
+        }
+        # Otherwise
+        else {
+            # Add user to the specified identity group
+            Add-ADGroupMember -Identity $_.Identity -Members $_.Username
+        }
+        try {$group = Get-ADGroup -Identity $_.Department}
+        catch {"$_.Department group does not exist. Creating group."}
+        if ($group.SamAccountName -eq $_.Department) {
+            # Add the current user to the group
+            Add-ADGroupMember -Identity $_.Department -Members $_.Username
+        }
+        # Otherwise create the group
+        else {
+            try {
+            New-ADGroup `
+                -SamAccountName $_.Department `
+                -Name $_.Department `
+                -GroupScope Universal
+            # Add the user to the group
+            Add-ADGroupMember -Identity $_.Department -Members $_.Username
+            } catch { "Cannot create a group with the same SAM Account Name."}
+        }       
 }
