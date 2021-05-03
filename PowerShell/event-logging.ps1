3eb7e3c79d2de7500361df72407b8604e9c8058a
Memeklos <59892644+Memeklos@users.noreply.github.com>
2021-05-02 22:21:45 -0500

    Create event-logging.ps1

---
+++ PowerShell/event-logging.ps1
@@ -0,0 +1,10 @@
+
+# Print a list of available eventlogs
+# Set up a new event log ( name it however you like )
+# Set up the log to have a max of 64Kb
+# Only 1 day of retention
+# Overflow action of OverwriteOlder
+# Write 100 logs to it
+# Print the list of available eventlogs again
+# Export the event log to a file
+# Clear the event log
