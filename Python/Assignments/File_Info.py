97f3ef747202962407996068d164d0a03433e6d3
Memeklos <mariel.klosterman@trojans.dsu.edu>
2021-05-06 08:29:04 -0700

    Completing final touches

--- Python/Assignments/File_Info.py
+++ Python/Assignments/File_Info.py
@@ -7,6 +7,7 @@ import argparse
 import json
 import sys
 import os
+import time
 
 ## Creates Argparse ##
 # Creates the parser object
@@ -16,7 +17,7 @@ parser.add_argument('-p', '--path', help='the directory to be parsed', required=
 try:
     # Sets the variable m to parse_args of parser
     m = parser.parse_args()
-    print(m)
+    path = m.path
 
 except:
     # Prints the help section
@@ -25,25 +26,35 @@ except:
 
 ## Collect and Print Information ##
 # Declare dictionary
-files = {}
+data = {}
 data['files'] = []
-# Takes info and creates a JSON object for each file (You’ll decide on the structure of this object)
-data['files'].append({
-    'name': 'Scott',
-    'size': 'stackabuse.com',
-    'modified': 'Nebraska'
-})
 
+# Sets the dire variable to the output of os.scandir
+dire = os.scandir(path)
 
-os.chdir(m)
-os.scandir(m)
+# Loops through the directory
+for i in dire :
+    # Checks if the item is a file
+    if i.is_file():
+        # Sets the fname variable to the file's name
+        fname = i.name
+        # Sets the modtime variable to the file's modified time
+        modtime = time.ctime(os.path.getmtime(i))
+        # Sets the size variable to the file's size in bytes
+        size = os.path.getsize(i)
+        # Takes info and creates a JSON object for each file
+        data['files'].append({
+            'filename': fname,
+            'size': size,
+            'modified': modtime
+        })
+dire.close()
+
 # Dumps all the file JSON objects to a JSON file
 # Opens JSON file
 with open('file-attributes.json', 'w') as outfile:
     # Dumps to JSON file
-    json.dump(files, outfile)
-#f = open('file-attributes.json', "w")
+    json.dump(data, outfile)
 
 # Close file
-#f.close()
-
+outfile.close()
